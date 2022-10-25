17e264b16f5e075a821f5fa37972a1a794fbc6e8
LIN115 <113489076+LIN115@users.noreply.github.com>
2022-10-25 18:55:53 +0800

    Update Class18.java

--- Class18.java
+++ Class18.java
@@ -3,7 +3,7 @@ package Java09;
 public class Class18 {
 
 		public static void main(String args[]) {
-			(new CCircle(2.0) { //
+			(new CCircle(2.0) { //匿名內部類別CCircle
 				void show() {
 					System.out.println("area=" + pi * radius * radius);
 				}
